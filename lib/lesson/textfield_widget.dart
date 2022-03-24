fb5e551da3839392705a0b6d608f0f8effafc81a
Peter Sam <sam.krealogi@gmail.com>
2022-03-24 13:51:22 +0700

    textField decoration

--- lib/lesson/textfield_widget.dart
+++ lib/lesson/textfield_widget.dart
@@ -21,11 +21,20 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               TextField(
+                decoration: InputDecoration(
+                    icon: Icon(Icons.login),
+                    prefixIcon: Icon(Icons.lock),
+                    prefixText: 'prefixText: ',
+                    labelText: 'labelText',
+                    hintText: 'hint',
+                    suffix: Container(width: 5, height: 5, color: Colors.red),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    filled: true,
+                    fillColor: Colors.green.withOpacity(0.5)),
                 controller: controller,
                 onChanged: (value) {
-                  setState(() {
-
-                  });
+                  setState(() {});
                 },
                 obscureText: true,
                 maxLength: 6,
