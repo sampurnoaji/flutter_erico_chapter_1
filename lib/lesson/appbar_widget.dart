6dd6f5aa2b4d921c534887ca6cedd8df70a1b123
Peter Sam <sam.krealogi@gmail.com>
2022-03-25 14:39:16 +0700

    custom appBar

--- lib/lesson/appbar_widget.dart
+++ lib/lesson/appbar_widget.dart
@@ -5,19 +5,22 @@ class AppBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(Lesson.AppBar_Widget.name),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.send)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.more)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.message)),
-        ],
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Color(0xff0096ff), Color(0xff6610f2)],
-                begin: FractionalOffset.topLeft,
-                end: FractionalOffset.bottomRight),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200),
+        child: AppBar(
+          title: Text(Lesson.AppBar_Widget.name),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.message)),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                  begin: FractionalOffset.topLeft,
+                  end: FractionalOffset.bottomRight),
+            ),
           ),
         ),
       ),
